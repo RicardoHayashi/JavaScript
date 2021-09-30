c2ff510d8f7ab1eb76f36d7f5b95c4e2b6d1af04
Ricardo Hayashi <hayashi.ricardo@gmail.com>
2021-09-30 15:38:55 +0000

    add even.js hello.js

---
+++ lighthouse-js-fundamentals/even.js
@@ -0,0 +1,8 @@
+const isEven = function (arg) {
+    return arg % 2 === 0;
+}
+
+console.log(isEven(10));
+
+console.log(isEven(5));
+
---
+++ lighthouse-js-fundamentals/hello.js
@@ -0,0 +1,11 @@
+const sayHello = function (arg) {
+    console.log("Hello, " + arg);
+}
+
+sayHello("Ricardo");
+
+sayHello("Eduardo");
+
+sayHello("Ellen");
+
+sayHello("Meire"); 
